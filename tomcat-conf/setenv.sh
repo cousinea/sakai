373e2dfde4690a7c85b06f29606acc484b995b32
Rick Carter <Rick.Carter@gmail.com>
2017-06-02 13:53:05 -0400

    tomcat mods

---
+++ tomcat-conf/setenv.sh
@@ -0,0 +1,1 @@
+export JAVA_OPTS="-server -Xmx1028m -XX:MaxMetaspaceSize=512m -Dorg.apache.jasper.compiler.Parser.STRICT_QUOTE_ESCAPING=false -Djava.awt.headless=true -Dcom.sun.management.jmxremote -Dhttp.agent=Sakai -Djava.util.Arrays.useLegacyMergeSort=true"
